3d0c89a064d1fbe393620d1bde98fe429a1add30
Jose Flores <jose.flores1634@gmail.com>
2018-03-27 15:27:25 -0700

    created the template and constructor function

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,38 +3,40 @@ var app = app || {};
 
 (function (module) {
 
-function Books (rawDataObj) {
-  this.title = rawDataObj.title;
-  this.author = rawDataObj.author;
-  this.isbn = rawDataObj.isbn;
-  this.image_url = rawDataObj.image_url;
-  this.description = rawDataObj.description;
-}
+  function Books (rawDataObj) {
+    this.title = rawDataObj.title;
+    this.author = rawDataObj.author;
+    this.isbn = rawDataObj.isbn;
+    this.image_url = rawDataObj.image_url;
+    this.description = rawDataObj.description;
+  }
 
-Books.all = [];
+  Books.all = [];
 
-Books.prototype.toHtml = function() {
-  let template = Handlebars.compile($something)
-  return template(this);
-}
+  Books.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template'))
+    return template(this);
+  }
 
-Books.loadAll = bookData;
+  Books.loadAll = rows => Books.all = rows.sort((a, b) => a.title - b.title).map(book => new Book(book));
 
-//FETCH ALL
-Books.fetchAll = () => {
-  if (localStorage.rawData) {
-    Books.loadAll(JSON.parse(localStorage.rawData));
+
+  //FETCH ALL
+  Books.fetchAll = () => {
+    if (localStorage.rawData) {
+      Books.loadAll(JSON.parse(localStorage.rawData));
       bookView.initIndexPage();
-  } else {
-    $.getJSON('http://localhost:3000/api/v1/books').then(results => console.log(results)).then(data => {
-      Books.loadAll(data);
-      localStorage.rawData = JSON.stringify(data);
+    } else {
+      $.getJSON('http://localhost:3000/api/v1/books').then(results => console.log(results)).then(data => {
+        Books.loadAll(data);
+        localStorage.rawData = JSON.stringify(data);
 
-      bookView.initIndexPage(); 
-  }, err => {
-      console.error(err);
-  });
-  }
+        bookView.initIndexPage();
+      }, err => {
+        console.error(err);
+      });
+    }
 
   }
+  module.Books = Books;
 })(app)
