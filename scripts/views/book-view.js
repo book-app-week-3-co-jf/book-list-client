52af6da9127336a282c5ebc5239bf895d4550bc4
Jose Flores <jose.flores1634@gmail.com>
2018-03-29 10:59:15 -0700

    added detail button

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -8,16 +8,15 @@ var bookView = {};
 bookView.initIndexPage = () => {
     app.Books.all.forEach(a => $('#title-author').append(a.toHtml()));
   }
-// need to edit the initDetailView 
+// need to edit the initDetailView
 //create hb template and pass the book thru template and append to the page
 bookView.initDetailView = (book) => {
-  $('.container').hide();  
-  $('.form-view').hide();
-  $('.detail-view').show(); 
+  $('#book-list-template').hide();
+  $('.detail-view-template').show();
+  var template = Handlebars.compile($('#detail-view-template').text());
   // let selected = Books.all.filter(el => el.book_id = params.book_id)   //this line needs edits
-  $('#book-detail-view').append(book); //this needs edited
+  $('.detail-view').append(book); //this needs edited
 }
-
 
   module.bookView = bookView;
 })(app);
